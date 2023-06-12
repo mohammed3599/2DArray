e3d272687cf5f120d9639ec557b42a606d22cd40
mohammed3599 <kataloni1999@gmail.com>
2023-06-12 11:00:48 +0400

    second commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2D_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] my2DArray = new string[3, 2]; // 2D array
+            my2DArray[0, 0] = "00";
+            my2DArray[1, 0] = "10";
+            my2DArray[2, 0] = "20";
+            my2DArray[0, 1] = "01";
+            my2DArray[1, 1] = "11";
+            my2DArray[2, 1] = "21";
+
+            Console.WriteLine(my2DArray.Length);
+            Console.WriteLine();
+
+            for (int row = 0;  row < my2DArray.GetLength(0); row++)
+            {
+                for(int col = 0; col < my2DArray.GetLength(1); col++)
+                {
+                    Console.Write(my2DArray[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
